c747841ab7127ae1b9719eb5336d07a0fc41cd8e
holawaleh <holawaleh@gmail.com>
2026-07-27 17:02:58 +0100

    ssid changed

--- _backup_include/secrets.h
+++ _backup_include/secrets.h
@@ -3,8 +3,8 @@
 #define AP_SSID      "smart_timer_AP"
 #define AP_PASSWORD  "smarttimer1"
 
-#define STA_SSID     "Tech Affairs"
-#define STA_PASSWORD " blackberry  "
+#define STA_SSID     "unilorin"
+#define STA_PASSWORD "unilorin123"
 
 #define CLOUD_API_BASE "https://iotbasedexamtimer.vercel.app"
 #define DEVICE_SECRET  "xk9m2pQ7vT4nR8wL5cJ1sB6hY3dF0aE"
